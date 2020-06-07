d3874edf1379461afa5337528bbef9562190f054
ikizey <ikizey@gmail.com>
2020-06-07 19:38:10 +0300

    added amath app, added urls.py, added templates/amath. +1st view @ /amath/

--- djmath/amath/views.py
+++ djmath/amath/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    # return HttpResponse("<h1>test</h1>")
+    return render(request, "amath/index.html")
